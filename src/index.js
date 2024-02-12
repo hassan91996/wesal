5c99e41d14c715f61442adffb587ccead809935d
hassan91996 <hassankhaled91996@gmail.com>
2024-02-12 22:32:56 +0200

    edits

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import Layout from './Layout';
 import App from './App';
 import './i18n'
 import reportWebVitals from './reportWebVitals';
